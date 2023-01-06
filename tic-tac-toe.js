60aa5702fdfe3924db6a741748bd6e626dc9a9bc
Selina Lavery <selina118@hotmail.com>
2023-01-06 11:47:54 +0000

    beginnings of pseudocode as comments in js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,16 @@
-console.log("lets smash this!!");
+/*PSEUDOCODE
+Press play button:
+-game starts
+-board resets
+-display shows who's turn it is
+
+Click board:
+-when grid is clicked it should display X or O
+-display should indicate turn
+-play button greys out
+
+Winner:
+-when 3 in a row
+-display the winner
+-play button activates
+*/
